194bc149d03d2f9af06700ad798677686a454937
brian <bsipin@gmail.com>
2024-03-28 20:53:13 -0400

    adding template

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into customers (name) values ('Brian');
+insert into customers (name) values ('Alice');
+insert into brackets (name) values ('Brians bracket');
+insert into brackets (name) values ('Alices bracket');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE customers (
+                           id SERIAL PRIMARY KEY,
+                           name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE brackets (
+                          id SERIAL PRIMARY KEY,
+                          name VARCHAR(255) NOT NULL
+);
