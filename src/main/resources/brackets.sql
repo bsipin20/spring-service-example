abed6b7892f81703f5268cbf14962cce88af2006
brian <bsipin@gmail.com>
2024-06-02 14:11:00 -0400

    new

---
+++ src/main/resources/brackets.sql
@@ -0,0 +1,5 @@
+CREATE TABLE brackets (
+                          id SERIAL PRIMARY KEY,
+                          name VARCHAR(255) NOT NULL,
+                          customer_id INT NOT NULL
+);
